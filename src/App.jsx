827e3c7b70b04d4e0bffffb8a3aea6eb9ccad5c5
Joseph Selwan <josephselwan2000@hotmail.com>
2026-01-23 23:00:49 +0200

    Test Toggle Lips Image 5

--- src/App.jsx
+++ src/App.jsx
@@ -295,7 +295,7 @@ function App() {
       <section className="relative px-6 py-24">
         <div className="max-w-4xl mx-auto">
           {/* Lips Image */}
-          {/* COMMENTED OUT - Lips Image
+          
           <div className="text-center mb-20">
             <div className="inline-block" style={{
               filter: 'drop-shadow(0 0 30px rgba(168, 85, 247, 0.6)) drop-shadow(0 0 60px rgba(168, 85, 247, 0.3))'
@@ -311,7 +311,6 @@ function App() {
               />
             </div>
           </div>
-          */}
           
 
           {/* CTA Content */}
